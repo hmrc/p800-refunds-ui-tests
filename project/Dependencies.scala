482ceda63c20eaa233fc90f148530000821cad4a
DarrenCB <58850786+DarrenCB@users.noreply.github.com>
2023-11-28 11:08:45 +0000

    [DCB] update cucumber-scala to 8.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     "com.vladsch.flexmark" % "flexmark-all"      % "0.62.2"   % Test,
     "org.scalatest"       %% "scalatest"         % "3.2.17"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"     % "3.2.17.0" % Test,
-    "io.cucumber"         %% "cucumber-scala"    % "8.18.1"   % Test,
+    "io.cucumber"         %% "cucumber-scala"    % "8.19.0"   % Test,
     "io.cucumber"          % "cucumber-junit"    % "7.14.1"   % Test,
     "junit"                % "junit"             % "4.13.2"   % Test,
     "com.novocode"         % "junit-interface"   % "0.11"     % Test,
