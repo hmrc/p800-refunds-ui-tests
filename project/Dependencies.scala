61bf1e2acc874dbe267befb95313001e3999e32e
DarrenCB <58850786+DarrenCB@users.noreply.github.com>
2024-07-22 14:49:03 +0100

    [DCB] organise feature files & dependency updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,11 +7,11 @@ object Dependencies {
     "com.vladsch.flexmark" % "flexmark-all"       % "0.62.2"   % Test,
     "org.scalatest"       %% "scalatest"          % "3.2.19"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"      % "3.2.17.0" % Test,
-    "io.cucumber"         %% "cucumber-scala"     % "8.23.0"   % Test,
-    "io.cucumber"          % "cucumber-junit"     % "7.18.0"   % Test,
+    "io.cucumber"         %% "cucumber-scala"     % "8.23.1"   % Test,
+    "io.cucumber"          % "cucumber-junit"     % "7.18.1"   % Test,
     "junit"                % "junit"              % "4.13.2"   % Test,
     "com.novocode"         % "junit-interface"    % "0.11"     % Test,
-    "org.mongodb.scala"   %% "mongo-scala-driver" % "5.1.1"    % Test,
+    "org.mongodb.scala"   %% "mongo-scala-driver" % "5.1.2"    % Test,
     "uk.gov.hmrc"         %% "ui-test-runner"     % "0.31.0"   % Test
   )
 
